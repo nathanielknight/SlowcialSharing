e1b38386cd4f330ee3428399417fd9b20d663f47
Nat Knight <nathaniel.ep@gmail.com>
2025-03-02 20:15:26 -0800

    Saving little edits

---
+++ scripts/init.sql
@@ -0,0 +1,3 @@
+INSERT INTO Sites (name, homepageurl, lastchecked, rssurl) VALUES
+    ('news.ycombinator.com', 'https://news.ycombinator.com', null, 'https://news.ycombinator.com/rss'),
+    ('lobste.rs', 'https://lobste.rs', null, 'https://lobste.rs/rss');
