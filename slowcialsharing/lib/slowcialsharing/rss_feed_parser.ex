101e99abff19f5dbc5bc431fd186c64c9565a804
Nat Knight <nathaniel.ep@gmail.com>
2025-03-03 23:20:12 -0800

    Rss Feed Parsing

---
+++ slowcialsharing/lib/slowcialsharing/rss_feed_parser.ex
@@ -0,0 +1,42 @@
+defmodule Slowcialsharing.RssFeedParser do
+  alias Slowcialsharing.Item
+
+  def parse_feed(feed) do
+    case Floki.parse_document(feed) do
+      {:ok, doc} ->
+        get_items_from_rss(doc)
+
+      {:error, reason} ->
+        {:error, "RSS parsing failed: ${inspect(reason)}"}
+    end
+  end
+
+  def get_items_from_rss(doc) do
+    case Floki.find(doc, "rss") do
+      [{"rss", _, _}] ->
+        get_items(doc)
+
+      _ ->
+        {:error, "Not an rss feed"}
+    end
+  end
+
+  def get_items(doc) do
+    rss_items = Floki.find(doc, "item")
+    items = for rss_item <- rss_items, do: parse_item(rss_item)
+    {:ok, items}
+  end
+
+  defp parse_item({"item", _attrs, children}) do
+    {"link", _, [link]} = find_element(children, "link")
+    {"title", _, [title]} = find_element(children, "title")
+    {"guid", _, [key]} = find_element(children, "guid")
+    {"comments", _, [commentslink]} = find_element(children, "comments")
+    %{link: link, title: title, key: key, commentslink: commentslink}
+  end
+
+  defp find_element(elements, name) do
+    elements
+    |> Enum.find(fn {n, _, _} -> n == name end)
+  end
+end
---
+++ slowcialsharing/test/slowcialsharing/rss_item_parser_test.exs
@@ -0,0 +1,66 @@
+defmodule Slowcialsharing.RssFeedParserTest do
+  alias Slowcialsharing.RssFeedParser
+  use ExUnit.Case
+
+  test "reports nonsense input" do
+    {:error, _} = RssFeedParser.parse_feed("")
+  end
+
+  test "parses rss feeds" do
+    rss_feed = """
+    <?xml version="1.0" encoding="UTF-8"?>
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+      <title>lobste.rs</title>
+      <title>Lobsters</title>
+      <link>https://lobste.rs/</link>
+      <atom:link href="https://lobste.rs/rss" rel="self"/>
+      <description></description>
+      <pubDate>Mon, 03 Mar 2025 16:14:12 -0600</pubDate>
+      <ttl>120</ttl>
+    <item>
+      <title>Use Firefox in 2025</title>
+      <link>https://alexn.org/blog/2025/03/03/use-firefox/</link>
+      <guid>https://lobste.rs/s/np4prk</guid>
+      <author>alexn.org by alexelcu</author>
+      <pubDate>Mon, 03 Mar 2025 16:14:12 -0600</pubDate>
+      <comments>https://lobste.rs/s/np4prk/use_firefox_2025</comments>
+      <description>&lt;p&gt;&lt;a href="https://lobste.rs/s/np4prk/use_firefox_2025"&gt;Comments&lt;/a&gt;&lt;/p&gt;</description>
+      <category>browsers</category>
+      <category>android</category>
+      <category>privacy</category>
+      <category>web</category>
+    </item>
+    <item>
+      <title>Apple's Software Quality Crisis: When Premium Hardware Meets Subpar Software</title>
+      <link>https://www.eliseomartelli.it/blog/2025-03-02-apple-quality</link>
+      <guid>https://lobste.rs/s/epok9v</guid>
+      <author>eliseomartelli.it by eliseomartelli</author>
+      <pubDate>Mon, 03 Mar 2025 02:10:07 -0600</pubDate>
+      <comments>https://lobste.rs/s/epok9v/apple_s_software_quality_crisis_when</comments>
+      <description>&lt;p&gt;&lt;a href="https://lobste.rs/s/epok9v/apple_s_software_quality_crisis_when"&gt;Comments&lt;/a&gt;&lt;/p&gt;</description>
+      <category>ios</category>
+      <category>rant</category>
+    </item>
+    </channel>
+    </rss>
+    """
+
+    assert {:ok, items} = RssFeedParser.parse_feed(rss_feed)
+
+    assert items == [
+             %{
+               commentslink: "https://lobste.rs/s/np4prk/use_firefox_2025",
+               key: "https://lobste.rs/s/np4prk",
+               link: "https://alexn.org/blog/2025/03/03/use-firefox/",
+               title: "Use Firefox in 2025"
+             },
+             %{
+               commentslink: "https://lobste.rs/s/epok9v/apple_s_software_quality_crisis_when",
+               key: "https://lobste.rs/s/epok9v",
+               link: "https://www.eliseomartelli.it/blog/2025-03-02-apple-quality",
+               title:
+                 "Apple's Software Quality Crisis: When Premium Hardware Meets Subpar Software"
+             }
+           ]
+  end
+end
