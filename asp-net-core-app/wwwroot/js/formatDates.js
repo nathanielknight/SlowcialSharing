b973e2db0773daf6f42e87cdbb5b801899efb5b0
Nat Knight <nathaniel.ep@gmail.com>
2025-03-05 22:25:32 -0800

    Update About page

--- asp-net-core-app/wwwroot/js/formatDates.js
+++ asp-net-core-app/wwwroot/js/formatDates.js
@@ -1,12 +1,13 @@
-const dtElements = document.querySelectorAll("[data-datetime]");
 const options = {
     dateStyle: "medium",
     timeStyle: "medium",
 };
-function reformatElement(e) {
 
+function reformatElement(e) {
     const src = e.attributes["data-datetime"].value;
     const dt = new Date(Date.parse(src));
     e.innerText = dt.toLocaleString(navigator.language, options);
 }
+
+const dtElements = document.querySelectorAll("[data-datetime]");
 dtElements.forEach(reformatElement);
