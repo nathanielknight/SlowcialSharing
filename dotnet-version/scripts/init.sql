f671869f3d984fe026cfc3607da6ce7bc3daf722
Nat Knight <nathaniel.ep@gmail.com>
2026-04-08 16:05:02 -0700

    remove dotnet version

--- dotnet-version/scripts/init.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Sites (name, homepageurl, lastchecked, rssurl) VALUES
-    ('news.ycombinator.com', 'https://news.ycombinator.com', null, 'https://news.ycombinator.com/rss'),
-    ('lobste.rs', 'https://lobste.rs', null, 'https://lobste.rs/rss');
